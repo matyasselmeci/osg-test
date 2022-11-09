06b8e4f83f37ca9caa13e06ebe46f34f4dae1c0f
Matyas Selmeci <matyas@cs.wisc.edu>
2022-11-09 11:47:09 -0600

    Try Overwrite: F for the TPC test

--- osgtest/tests/test_465_xrootd_tpc.py
+++ osgtest/tests/test_465_xrootd_tpc.py
@@ -46,7 +46,8 @@ class TestXrootdTPC(osgunittest.OSGTestCase):
     def copy_command(self, source_url, dest_url, source_token=None, dest_token=None):
         command = ["curl", "-A", "Test", "-vk", "-X", "COPY",
                    "-H", f"Source: {source_url}",
-                   "-H", "Overwrite: T",
+                   #"-H", "Overwrite: T",
+                    "-H", "Overwrite: F",
         ]
         # We're contacting the destination which means we need to auth to the
         # destination and give it the token to auth to the source 
