6baf03a74b18d651b9a11d6d280a6effe3a05f33
Matyas Selmeci <matyas@cs.wisc.edu>
2023-08-17 15:33:06 -0500

    DEBUG: do a cconfig dump of xrootd on startup

--- osgtest/library/xrootd.py
+++ osgtest/library/xrootd.py
@@ -1,5 +1,6 @@
 """Utilities for dealing with XRootD"""
 import os
+from typing import List
 
 from osgtest.library import core
 
@@ -7,20 +8,27 @@ from osgtest.library import core
 ROOTDIR = f"/tmp/xrootd-osgtest-{os.getpid()}"
 
 
-def cconfig(instance, executable="xrootd", raw=True, quiet=True):
-    """Return the a config dump of an xrootd instance using cconfig"""
+def cconfig_raw(instance, executable="xrootd", quiet=True) -> str:
+    """Return the a config dump of an xrootd instance using cconfig, as a
+    single string.
+    """
     # Using shell=True because cconfig prints output into stderr and there's a
     # bug in core.__run_command()'s stderr handling.
     ret, output, _ = core.system(f"cconfig -x {executable} -n {instance} -c /etc/xrootd/xrootd-{instance}.cfg 2>&1", shell=True, quiet=quiet)
 
-    if raw:
-        if ret != 0:
-            return ""
-        return output
-    else:
-        if ret != 0:
-            return []
-        return [line for line in output.splitlines() if not line.startswith("Config continuing with file ")]
+    if ret != 0:
+        return ""
+    return output
+
+
+def cconfig(instance, executable="xrootd", quiet=True) -> List[str]:
+    """Return the a config dump of an xrootd instance using cconfig, as a processed
+    list with non-config lines removed.
+    """
+    raw_output = cconfig_raw(instance, executable, quiet)
+    if not raw_output:
+        return []
+    return [line for line in raw_output.splitlines() if not line.startswith("Config continuing with file ")]
 
 
 def dump_log(lines, instance, executable="xrootd"):
